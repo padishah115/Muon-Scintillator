9e4235a0ea059f4296b221a6517e405e19c70e31
hayde <hayde@LAPTOP-52LE53OM>
2024-02-27 21:48:02 +0000

    fun with energy distributions i guess

--- Energy Distribution/energy_distribution_graphical_plot.py
+++ Energy Distribution/energy_distribution_graphical_plot.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+x = np.arange(5,300, 5)
+means = []
 
 class Energy_Distribution:
 
@@ -10,13 +15,12 @@ class Energy_Distribution:
         self.emax = emax
         self.e_vals = np.linspace(self.emin, self.emax)
         self.p_values = self.get_p_values()
-
-        self.fig, self.ax = plt.subplots()
+        self.mean = self.get_mean_energy()
         
     def get_p_values(self):
         """Returns the associated probabilities for each of the muons in the distributions"""
         p = (((4.290 + self.e_vals)**-3.01) / ((1 + self.e_vals/854)))
-        p = p / np.sum(p)
+        p = np.multiply(p, 1/ np.sum(p))
 
         return p
     
@@ -28,23 +32,38 @@ class Energy_Distribution:
 
     
     def plot_distribution(self):
-        plt.plot(self.e_vals, self.p_values, label = f'Mean energy = {self.get_mean_energy():.2f} GeV')
-        plt.legend()
+        ax.plot(self.e_vals, self.p_values, label = f'Mean energy = {self.mean:.2f} GeV')
+        ax.legend()
+        ax.set_xlabel('Energy / GeV')
+        ax.set_ylabel('Probability of Energy')
         plt.title(f'Energy Probability Function. Min: {self.emin:.2f} GeV. Max: {self.emax:.2f} GeV')
-        plt.xlabel('Energy / GeV')
-        plt.ylabel('Probability of Energy')
-        plt.savefig(f'energy_distribution_graph_{self.emin:.2f}_{self.emax:.2f}_GeV.png')
-        plt.show()
-        
-
         
 
 
 e_min = 0.1 #100 MeV
-e_max = 300 #300 GeV
+e_maxes = [i for i in x]
 
-distribution1 = Energy_Distribution(e_min, e_max)
-distribution1.plot_distribution()
+for emax in e_maxes:
+    distrib = Energy_Distribution(e_min, emax)
+    mean = distrib.mean
+    means.append(mean)
 
+plt.plot(x, means)
+plt.title(f'Mean Energy as a Function of Maximum Energy. Min Energy: {e_min} GeV')
+plt.xlabel('Max Energy / GeV')
+plt.ylabel('Probability')
+plt.savefig('max_energy_vs_mean.png')
+plt.show()
 
+# def animate(i):
+#     ax.clear()
+#     e_max = e_maxes[i]
+#     distribution1 = Energy_Distribution(e_min, e_max)
+#     distribution1.plot_distribution()
+#     return 0
 
+# anim = animation.FuncAnimation(fig, animate, frames = len(e_maxes), interval = 1000)
+
+# plt.show()
+
+# anim.save('energy_distribution_animation_2.gif')
